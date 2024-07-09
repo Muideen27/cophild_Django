2bab387a4f08bf3e174ec56d3c735dc424ca0938
Muideen27 <ilorimuideen0000@gmail.com>
2024-07-09 11:51:20 +0100

    Adding up some changes in the views

--- SAIL_alumni/sailors/views.py
+++ SAIL_alumni/sailors/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from .models import Sailor
 
 def sailors(request):
-    template = loader.get_template('home.html')
-    return HttpResponse(template.render())
+    all_sailors = Sailor.objects.all().values()
+    template = loader.get_template('all_sailors.html')
+    context = {
+        'all_sailors': all_sailors,
+    }
+    return HttpResponse(template.render(context, request))

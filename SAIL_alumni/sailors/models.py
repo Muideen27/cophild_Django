202abdb29c27d9ba9849d1a3aa7a11f7005dbeab
Muideen27 <ilorimuideen0000@gmail.com>
2024-07-09 03:40:06 +0100

    files modified d

--- SAIL_alumni/sailors/models.py
+++ SAIL_alumni/sailors/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
-def sign_up:
-    f
-
-s Create your models here.
+# Create your models here.
--- SAIL_alumni/sailors/urls.py
+++ SAIL_alumni/sailors/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('sailors', views.sailors, name='sailors'),
+        path('', views.sailors, name='sailors'),
 ]

ed9c6a21e5d458d9ada397b480c9551e9928ac52
Muideen27 <ilorimuideen0000@gmail.com>
2024-07-09 10:30:31 +0100

    remodeling the model.py file and making the necessary migraton.

--- SAIL_alumni/sailors/models.py
+++ SAIL_alumni/sailors/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Sailor(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    date_of_birth = models.DateField(null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    graduation_year = models.IntegerField(null=True)
+    major = models.CharField(max_length=255, blank=True, null=True)
+    employment_status = models.CharField(
+            max_length=255,
+            choices=[
+                ('employment', 'Employed'),
+                ('unemployed', 'Unemployed'),
+                ('student', 'Student'),
+                ('freelance', 'Freelance'),
+                ('self-employed', 'Self-employed'),
+                ('other', 'Other')
+            ],
+            blank=True,
+            null=True
+    )
+    job_title = models.CharField(max_length=255, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"

68fefed2b0a5d1e64e66b914200c0f5e3d1eebf2
Muideen27 <ilorimuideen0000@gmail.com>
2024-07-09 03:36:31 +0100

    file modified to remder templates

--- SAIL_alumni/sailors/views.py
+++ SAIL_alumni/sailors/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-def sailors(response):
-    return HttpResponse("Hello world")
+
+def sailors(request):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())

c091ccbde5b0ce7d49eee865707d9e6d95a208c5
Muideen27 <ilorimuideen0000@gmail.com>
2024-07-15 02:49:30 +0100

    Adding model class to admin

--- SAIL_alumni/sailors/admin.py
+++ SAIL_alumni/sailors/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Sailor, Profile, Connection
 
 # Register your models here.
+admin.site.register(Sailor)
+admin.site.register(Profile)
+admin.site.register(Connection)

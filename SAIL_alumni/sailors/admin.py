bb8a7a7960191a2f2728df6c621145e3e1b13712
Muideen27 <ilorimuideen0000@gmail.com>
2024-07-15 11:16:25 +0100

    Registering Models inside the admin to easy superuser access

--- SAIL_alumni/sailors/admin.py
+++ SAIL_alumni/sailors/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Sailor, Profile, Connection
+from .models import Sailor, Profile, Connection, Education, Experience, Skill, Post, Comment, Like, Share, Group, GroupMember
 
-# Register your models here.
 admin.site.register(Sailor)
 admin.site.register(Profile)
 admin.site.register(Connection)
+admin.site.register(Education)
+admin.site.register(Experience)
+admin.site.register(Skill)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Share)
+admin.site.register(Group)
+admin.site.register(GroupMember)
+

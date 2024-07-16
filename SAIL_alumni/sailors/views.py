a47ead0787fa47b55d4aa722fedf0942b5fa9aff
Muideen27 <ilorimuideen0000@gmail.com>
2024-07-16 13:17:54 +0100

    creating a view for the serializer to allow react consume the request

--- SAIL_alumni/sailors/views.py
+++ SAIL_alumni/sailors/views.py
@@ -1,12 +1,55 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Sailor
+from . models import *
+from . serializer import *
+from rest_framework import viewsets
 
-def sailors(request):
-    all_sailors = Sailor.objects.all().values()
-    template = loader.get_template('all_sailors.html')
-    context = {
-        'all_sailors': all_sailors,
-    }
-    return HttpResponse(template.render(context, request))
+class SailorViewSet(viewset.ModelViewSet):
+    queryset = Sailor.objects.all()
+    serializer_class = SailorSerializer
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+class ConnectionViewSet(viewsets.ModelViewSet):
+    queryset = Connection.objects.all()
+    serializer_class = ConnectionSerializer
+
+class EducationViewSet(viewsets.ModelViewSet):
+    queryset = Education.objects.all()
+    serializer_class = EducationSerializer
+
+class ExperienceViewSet(viewsets.ModelViewSet):
+    queryset = Experience.objects.all()
+    serializer_class = ExperienceSerializer
+
+class SkillViewSet(viewsets.ModelViewSet):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = likeSerializer
+
+class ShareViewSet(viewsets.ModelViewSet):
+    queryset = Share.objects.all()
+    serializer_class = ShareSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class GroupMemberViewSet(viewsets.ModelViewSet):
+    queryset = GroupMember.objects.all()
+    serializer_class = GroupMemberSerializer
+

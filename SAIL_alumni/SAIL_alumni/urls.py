a6f3c2d7351bc14fa84bbccedc7d22373fe218b5
Muideen27 <ilorimuideen0000@gmail.com>
2024-07-28 21:33:43 +0100

    updating the sail_alumni project in the urls and the templates

--- SAIL_alumni/SAIL_alumni/urls.py
+++ SAIL_alumni/SAIL_alumni/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+
 
 urlpatterns = [
-    path('', include('sailors.urls')),
     path('admin/', admin.site.urls),
-]
+    path('api/', include('sailors.urls')),
+    path('', TemplateView.as_view(template_name='all_sailors.html')),  # Catch-all route for React
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- SAIL_alumni/sailors/urls.py
+++ SAIL_alumni/sailors/urls.py
@@ -1,5 +1,5 @@
-# urls.py
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 from rest_framework import routers
 from .views import SailorViewSet, ProfileViewSet, PostViewSet
 
@@ -9,7 +9,7 @@ router.register(r'profiles', ProfileViewSet)
 router.register(r'posts', PostViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),  # Catch-all route
 ]
-

90e638ba78b72030044e3738d58f617767bd32f1
Muideen27 <ilorimuideen0000@gmail.com>
2024-07-28 13:32:34 +0100

    creating URLs for API endpoints

--- SAIL_alumni/sailors/urls.py
+++ SAIL_alumni/sailors/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
-from . import views
+# urls.py
+from django.urls import include, path
+from rest_framework import routers
+from .views import SailorViewSet, ProfileViewSet, PostViewSet
+
+router = routers.DefaultRouter()
+router.register(r'sailors', SailorViewSet)
+router.register(r'profiles', ProfileViewSet)
+router.register(r'posts', PostViewSet)
 
 urlpatterns = [
-        path('', views.sailors, name='sailors'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
